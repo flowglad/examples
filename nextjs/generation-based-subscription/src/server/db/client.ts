751bf2726690f3c24c3b55523c007c1e916d1668
Liam Monaghan <liam.monaghan@flowglad.com>
2025-12-31 00:01:32 -0500

    moved nextjs examples to their own folder

--- nextjs/generation-based-subscription/src/server/db/client.ts
+++ nextjs/generation-based-subscription/src/server/db/client.ts
@@ -1,5 +1,5 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
-import { Pool } from 'pg';
+import { Pool } from 'node_modules/@types/pg';
 import * as schema from './schema';
 
 if (!process.env.DATABASE_URL) {
